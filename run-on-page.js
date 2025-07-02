7ead51ac21ec5eae46c97ddf46d47523151c48ad
asterite <asterite@seclab.cs.msu.ru>
2025-07-02 23:29:08 +0300

    Bump Puppeteer and improve reliability
    Update the version of Puppeteer and make the code more stable by
    waiting for all script sources requested
    by 'Debugger.getScriptSource' to arrive and by using a flag to
    disabling requesting of new scripts when we have processed the
    scripts and are about to close the browser.

--- run-on-page.js
+++ run-on-page.js
@@ -9,25 +9,34 @@ const url = process.argv.length >= 3 ? process.argv[2] : 'https://www.solidpoint
     const page = await browser.newPage();
 
     const pageScripts = [];
-    const client = await page.target().createCDPSession();
+    const client = await page.createCDPSession();
 
     await client.send('Debugger.enable');
 
-    client.on('Debugger.scriptParsed', async function ({ scriptId, url }) {
-        const { scriptSource } = await client.send(
-            'Debugger.getScriptSource',
-              { scriptId }
-        );
-        pageScripts.push({ scriptSource, url });
+    const scriptPromises = [];
+    let finished = false;
+    client.on('Debugger.scriptParsed', function ({ scriptId, url }) {
+        if (finished) return;
+        async function getSource() {
+            const { scriptSource } = await client.send(
+                'Debugger.getScriptSource',
+                { scriptId }
+            );
+            pageScripts.push({ scriptSource, url });
+        }
+        const scriptAcquired = getSource();
+        scriptPromises.push(scriptAcquired);
     });
 
     await page.goto(url);
+    await Promise.all(scriptPromises);
 
-    for (const { url, scriptSource} of pageScripts) {
+    for (const { url, scriptSource }  of pageScripts) {
         console.log('analyzing script', url);
         analyze(scriptSource);
     }
 
+    finished = true;
 
     await browser.close();
 })();
